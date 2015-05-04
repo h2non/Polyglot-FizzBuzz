1d817bee1b5273a68a1bdce8f1f8f0bab9b84959
Tomas Aparicio <tomas@aparicio.me>
2015-05-04 20:36:06 +0200

    feat(js): add other functional example

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,10 +1,10 @@
 // Traditional imperative way with boilerplate
 
 for (var i = 0; i < 100; i += 1) {
-  if (i % 5 === 0 && i % 3 === 0) console.log('FizzBuzz')
-  else if (i % 5 === 0)           console.log('Buzz')
-  else if (i % 3 === 0)           console.log('Fizz')
-  else                            console.log(i)
+  if (i % 15 === 0)      console.log('FizzBuzz')
+  else if (i % 5 === 0)  console.log('Buzz')
+  else if (i % 3 === 0)  console.log('Fizz')
+  else                   console.log(i)
 }
 
 // Traditional procedural way with less boilerplace but relies on mutation
@@ -14,22 +14,37 @@ for (var i = 0; i < 100; i += 1) {
   out = ''
   if (i % 3 === 0) out += 'Bizz'
   if (i % 5 === 0) out += 'Fuzz'
-  if (!out)  out += i
+  if (!out)        out += i
   console.log(out)
 }
 
 // Functional declarative way
 
-var list = Array.apply(null, Array(100 + 1)).map(function (_, i) {
+function bizzbuzz(num) {
+  return (
+    (num % 3 === 0 ? 'Bizz' : '') +
+    (num % 5 === 0 ? 'Buzz' : '') +
+    (num % 15      ? num    : '')
+  )
+}
+
+var list = Array.apply(null, Array(100)).map(function (_, i) {
   return bizzbuzz(i)
 })
 
 console.log(list.join('\n'))
 
-function bizzbuzz(num) {
-  return (
-    (num % 3 === 0 ? 'Bizz' : '') +
-    (num % 5 === 0 ? 'Buzz' : '') +
-    (num % 15      ? num : '')
-  )
-}
+// Another functional-style approach (less concise, explicit and efficient)
+
+var pairs = { 3: 'Bizz', 5: 'Buzz', 15: 'BizzBuzz' }
+var keys = Object.keys(pairs)
+
+var out = Array.apply(null, Array(100)).map(function (_, i) {
+  return keys.map(function (num) {
+    return i % num === 0 ? pairs[num] : null
+  }).filter(function (num) {
+    return num
+  }).pop() || i
+})
+
+console.log(out.join('\n'))
